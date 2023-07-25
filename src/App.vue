6b94582c88362427c0591752d0c71ff626add254
Prabhvir Babra <theprabab@gmail.com>
2023-07-25 16:22:00 -0700

    Create a club header

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <script>
+import Header from "./components/Header.vue"
+
+export default {
+  name: "app",
+  components: {
+    Header,
+  }
+}
 </script>
 
 <template>
-  <img alt="Vue logo" src="/site-logo.png">
-  <div id="nav">
-    <h1>Routing</h1>
-    <router-link to="/"> Home </router-link> |
-    <router-link to="/about"> About </router-link>
-    <router-link to="/learn"> Learn </router-link>
-    <router-link to="/archive"> Archive </router-link>
-    <!-- TODO Add a Banner -->
-  </div>
+  <Header title="CPC Club" ></Header>
+
   <router-view></router-view>
+
 </template>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,53 @@
+<script>
+export default {
+  props: {
+    title: String,
+  }
+}
+</script>
+
+<template>
+  <div id="lefthand-menu">
+    <img alt="CPC Logo" src="/site-logo.png">
+    <h1 id="title"> <router-link to="/"> {{ title }} </router-link> </h1>
+  </div>
+
+  <div id="righthand-menu">
+    <router-link to="/about"> About </router-link>
+    <router-link to="/learn"> Learn </router-link>
+    <router-link to="/archive"> Archive </router-link>
+  </div>
+
+  <div id="site-banner">
+    <img alt="CPC Banner" src="/banner.png">
+  </div>
+</template>
+
+<style>
+:root {
+  --header-bar-height: 75px;
+}
+
+#lefthand-menu {
+    display: inline;
+}
+#lefthand-menu > img {
+    height: var(--header-bar-height);
+    width: auto;
+}
+#lefthand-menu > h1#title {
+    display: inline;
+}
+
+#righthand-menu {
+    display: inline;
+    float: right;
+}
+
+#site-banner {
+    width: 100%;
+}
+#site-banner > img {
+    width: 100%;
+}
+</style>
