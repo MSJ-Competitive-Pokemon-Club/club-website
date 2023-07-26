0d742a6cbbbd6c06c65233adec0b0ba895c928db
Prabhvir Babra <theprabab@gmail.com>
2023-07-26 16:51:26 -0700

    Finish basic Learn Page functionality

---
+++ src/components/LessonButton.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  props: {
+    lessonName: String,
+    replayLogLoc: String,
+  },
+  methods: {
+    sendReplayLoc() {
+      console.log(this.replayLogLoc);
+      console.log(this.lessonName);
+      this.$emit('replay-log-clicked', this.replayLogLoc);
+    }
+  }
+}
+</script>
+
+
+<template>
+  <div class="learn-button">
+    <!-- TODO Add an image to the side -->
+    <button @click="sendReplayLoc"> {{ lessonName }} </button>
+  </div>
+</template>
+
+<style>
+</style>
--- src/views/Learn.vue
+++ src/views/Learn.vue
@@ -1,5 +1,51 @@
+<script>
+  import LessonButton from "../components/LessonButton.vue"
+
+  import lessonData from "/src/assets/lessons/lessonInfo.json"
+
+  export default {
+    data() {
+      return {
+        categories: lessonData.categories,
+        currentLessonName: "Test",
+        currentReplayLoc: "src/assets/lessons/test.html",
+      }
+    },
+    components: {
+      LessonButton,
+    },
+    methods: {
+      changeReplayLog(replayLogLoc) {
+          console.log("In here");
+          console.log(replayLogLoc);
+          this.currentReplayLoc = replayLogLoc;
+      },
+    }
+  }
+</script>
+
 <template>
+
   <div class="learn">
-    <h1>Learn</h1>
+    <div id="learn-sidepanel">
+      <strong> Sidebar </strong>
+      <ul>
+        <li v-for="category in categories">
+          <h3> {{ category.name }} </h3>
+          <ul>
+            <li v-for="lesson in category.lessons">
+              <LessonButton :lessonName="lesson.name" :replayLogLoc="lesson.replaySrc" @replay-log-clicked="changeReplayLog"></LessonButton>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+
+    <div id="learn-display">
+      <h1>Learn</h1>
+      <h1> {{ this.lessonName }} </h1>
+      <iframe id="iframe" ref="battle" :src="currentReplayLoc" width="100%" height="600px" frameborder="0"></iframe>
+    </div>
+
   </div>
 </template>
