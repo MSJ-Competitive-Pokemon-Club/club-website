888abc2fddfcbf16c07933699ab391c02d71b055
qwertyryo <61341858+qwertyryo@users.noreply.github.com>
2023-07-31 17:58:05 -0700

    Update Learn.vue
    can select lesson from the side and display the replay on the right, needs fixing on appearance though

--- src/views/Learn.vue
+++ src/views/Learn.vue
@@ -9,41 +9,72 @@
         categories: lessonData.categories,
         currentLessonName: "Test",
         currentReplayLoc: "src/assets/lessons/test.html",
+        options: ['test','test2'],
+        selectedOption: null
       }
+      
     },
     components: {
       LessonButton,
     },
     methods: {
-      changeReplayLog(replayLogLoc) {
-          console.log("In here");
-          console.log(replayLogLoc);
-          this.currentReplayLoc = replayLogLoc;
-      },
+
     }
+    
   }
+
 </script>
 
 <template>
-  <div class="learn">
-    <div id="learn-sidepanel">
-      <strong> Sidebar </strong>
-      <ul>
-        <li v-for="category in categories">
-          <h3> {{ category.name }} </h3>
-          <ul>
-            <li v-for="lesson in category.lessons">
-              <LessonButton :lessonName="lesson.name" :replayLogLoc="lesson.replaySrc" @replay-log-clicked="changeReplayLog"></LessonButton>
-            </li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-
-    <div id="learn-display">
-      <h1>Learn</h1>
-      <h1> {{ this.lessonName }} </h1>
-      <iframe id="iframe" ref="battle" :src="currentReplayLoc" width="100%" height="600px" frameborder="0"></iframe>
+<div id="title">Learn</div>
+<div id="learn">
+  <div id="sidebar">
+    <div 
+      v-for="(option, index) in options"
+      :key="index"
+      :class="{ 'option': true, 'selected': selectedOption === index }"
+      @click="() => {
+        selectedOption = index;
+        currentReplayLoc = 'src/assets/lessons/'+ option + '.html';
+      }"
+    >
+      {{ option }}
     </div>
   </div>
+
+  <div id="learn-display">
+      <iframe id="iframe" ref="battle" :src="currentReplayLoc" width="100%" height="600px" frameborder="0"></iframe>
+  </div>
+</div>
 </template>
+<style scoped>
+
+#title{
+  font-size:150px;
+  text-align:center
+}
+#sidebar {
+    width:200px;
+    height: 10vh;
+    overflow-y: auto;
+    text-align: left;
+}
+
+#learn-display {
+  width:2000px;
+  height: 100vh;
+  overflow-y: auto;
+  text-align: right;
+}
+.option {
+    padding: 10px;
+    font-size: 12px;
+    cursor: pointer;
+}
+
+.option.selected {
+    font-size: 48px; /* Enlarge the selected option */
+    color:red
+}
+
+</style>
