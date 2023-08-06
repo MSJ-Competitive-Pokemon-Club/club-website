8dbb9da3bd035efc556a125c087864c8f8df3f3a
Prabhvir Babra <theprabab@gmail.com>
2023-08-06 15:06:14 -0700

    Hopefully fix spa issue

--- src/main.js
+++ src/main.js
@@ -10,12 +10,12 @@ import Archive from "@/views/Archive.vue"
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: "/club-website",         name: "Home",    component: Home },
-    { path: "/club-website/about",   name: "About",   component: About   },
-    { path: "/club-website/learn",   name: "Learn",   component: Learn   },
-    { path: "/club-website/archive", name: "Archive", component: Archive },
+    { path: "/club-website",         name: "Home",    component: Home,    mode: 'hash' },
+    { path: "/club-website/about",   name: "About",   component: About,   mode: 'hash' },
+    { path: "/club-website/learn",   name: "Learn",   component: Learn,   mode: 'hash' },
+    { path: "/club-website/archive", name: "Archive", component: Archive, mode: 'hash' },
+    { path: "*",                     name: "Home",    component: Home,    mode: 'hash' },
   ],
-  mode: 'hash'
 })
 
 createApp(App)
