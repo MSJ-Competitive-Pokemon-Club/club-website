7e814d2e0aad35e08af457c6c023e0d5020fce23
Alexander Lau <aljlau1220@gmail.com>
2023-08-14 18:21:22 -0700

    Minor Home.vue commenting
    Modified Home.vue with some minor commenting changes.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,5 @@
 <script>
+  // Imports should occur from src to avoid errors when running "npm run build"
   import welcome_message from "/src/welcome.txt?raw"
   import socialsData from "/src/config/social-buttons.json"
 
@@ -7,7 +8,7 @@
       return {
         welcome_message,
         socialsButtons: socialsData.socials,
-        CPC_logo: '/club-website/assets/images/CPC_logo_full.png',
+        CPC_logo: '/club-website/assets/images/CPC_logo_full.png', // The Public folder contains mostly static images.
         doorLoc: '/club-website/assets/images/door-open.png'
       }
     }
@@ -17,22 +18,19 @@
 <template>
   <div class="intro-section">
     <h1 id="site-title"> MSJ Competitive Pokemon Club </h1>
-    <div id="about_us">
-      <img class="logo" alt="CPC_logo" :src="CPC_logo">
+    <div id="about-us">
+      <img class="logo" alt="CPC-logo" :src="CPC_logo">
       <div id="who-are-we">
         <h2 id="subtitle"> Who Are We? </h2>
-        <div id="welcome-message-section"> {{ welcome_message }}</div>
+        <div id="welcome-message-section"> {{ welcome_message }} </div>
       </div>
     </div>
   </div>
   <div id="meeting-section">
     <div id="room-div">
-      <h2 class="room-text" id="room-subtext"> We Meet In Room B18. </h2>
-      <h2 class="room-text" id="join-us"> Come Join Us! </h2>
-    </div>
-    <div id="room-image">
-      <img alt="Meet us at B18!" :src="doorLoc">
-    </div>
+      <h2 id="room-subtext"> We Meet In Room B18. </h2>
+      <h2 id="join-us"> Come Join Us! </h2>
+    </div><img alt="Meet us at B18!" :src="doorLoc">
   </div>
   <div id="social_media">
     <h1>Here's our social media!</h1>
@@ -51,15 +49,14 @@
 </template>
 
 <style>
+
 #site-title {
-  text-align: center;
-  font-size: 4vw;
+  font-size: 4vw; /* vw stands for 'viewport width' */
   padding-bottom: 4%;
 }
 
-div#about_us {
+div#about-us {
   display: grid;
-  width: 100%;
   padding-left: 5%;
   padding-right: 5%;
   grid-template-columns: 50% 50%;
@@ -69,6 +66,7 @@ div#about_us {
 #who-are-we {
   margin-bottom: 4%;
 }
+
 div#welcome-message-section {
   white-space: pre-wrap;
   font-size: 2.4vw;
@@ -76,11 +74,13 @@ div#welcome-message-section {
   line-height:1.3;
   margin-left:3%;
 }
+
 #subtitle {
   text-align: center;
   font-size: 4vw;
   margin-bottom: 5%;
 }
+
 .logo {
   width: 100%;
   margin: auto;
@@ -98,10 +98,18 @@ div#welcome-message-section {
   padding-bottom: 4%;
   border-top: 2px solid black;
 }
+
+div#meeting-section > img {
+  width: 100%;
+  margin-left: 9.3%;
+}
+
+
 div#room-div {
   padding-top: 35%;
 }
-h2.room-text {
+
+div#room-div > h2 {
   font-size: 4.5vw;
   text-align: center;
   color: black;
@@ -111,31 +119,28 @@ div#social-media {
   margin: auto;
 }
 
-div#room-image > img {
-  width: 100%;
-  margin-left: 9.3%;
-}
-
 #social_media {
   text-align: center;
   padding-top: 7%;
   background: #283977;
   border-top: 2px solid black;
   border-bottom: 2px solid black;
-
 }
+
 #social_media > h1 {
   text-align: center;
   font-size: 4vw;
   padding-bottom: 5%;
   color: white;
 }
+
 ul#social-media-list > li {
   display: inline-block;
   width: 16vw;
   margin: 2vw;
   padding-bottom: 5%;
 }
+
 .social-button {
   display: grid;
   grid-template-columns: 20% 80%;
@@ -143,17 +148,21 @@ ul#social-media-list > li {
   padding: 1vw;
   background: #beb9d7;
 }
+
 ul#social-media-list > li > a {
   text-decoration: none;
   color: #283977;
 }
+
 ul#social-media-list > li > a:hover {
   text-decoration: underline;
 }
+
 .social-button > img {
   width: 100%;
   height: auto;
 }
+
 .social-button > h4 {
   font-size: 2vw;
 }
