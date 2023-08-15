acc2d359e0e47e715e1f244d948e8208c51bb065
Prabhvir Babra <theprabab@gmail.com>
2023-08-14 17:19:08 -0700

    Restyle + redraw header and site-logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,25 +8,24 @@ export default {
 
 <template>
   <div id="topbar">
-    <div></div>
     <router-link to="/club-website">
       <div id="lefthand-menu">
-        <img alt="CPC Logo" src="/site-logo.png">
+        <img alt="CPC Logo" src="/assets/images/site-logo.png">
         <h1 id="title">  {{ title }}  </h1>
       </div>
     </router-link>
 
     <ul id="righthand-menu">
-      <li> <router-link id="about-button" class="header-link" to="/club-website/about"> About 👥 </router-link> </li>
+      <li> <router-link id="crew-button" class="header-link" to="/club-website/crew"> Crew </router-link> </li>
       <li class="spacer"> | </li>
-      <li> <router-link id="learn-button" class="header-link" to="/club-website/learn"> Learn 🎓 </router-link> </li>
+      <li> <router-link id="learn-button" class="header-link" to="/club-website/learn"> Learn </router-link> </li>
       <li class="spacer"> | </li>
-      <li class="rightmost"> <router-link id="archive-button" class="header-link" to="/club-website/archive"> Archive 🗃️ </router-link> </li>
+      <li class="rightmost"> <router-link id="archive-button" class="header-link" to="/club-website/archive"> Archive </router-link> </li>
     </ul>
   </div>
 
   <div id="site-banner">
-    <img alt="CPC Banner" src="/banner.svg">
+    <img alt="CPC Banner" src="/assets/images/banner.svg">
   </div>
 
   <div id="ghost-element"></div>
@@ -35,10 +34,8 @@ export default {
 <style>
 div#topbar {
   display: grid;
-  grid-template-columns: 1% 29% 70%;
+  grid-template-columns: 50% 50%;
   background-image: linear-gradient(to right, #c9cbf5, #d6bcf0);
-  padding-top: 1%;
-  padding-bottom: 1%;
 }
 
 div#lefthand-menu {
@@ -50,27 +47,19 @@ div#lefthand-menu {
 }
 div#lefthand-menu > img {
   width: 80%;
-  margin: 10%;
+  margin: 5%;
 }
 h1#title {
   text-align: left;
-  font-size: 3.2vw;
+  font-size: 6vh;
 }
-<<<<<<< HEAD
 div#topbar a {
   text-decoration: none;
   color: black;
-  transition: transform 0.2s ease;
-=======
-#lefthand-menu > h1#title {
-    padding-top: 15px;
-    font-family: IPS-Medium;
->>>>>>> 7565cda (Use IBM Plex Sans as new font)
 }
 div#topbar a:hover {
+  text-decoration: underline;
   color: black;
-  transform: scale(1.1);
-  font-size: 3.2vw;
 }
 
 ul#righthand-menu {
@@ -86,37 +75,11 @@ ul#righthand-menu > li {
   display: flex;
   float: right;
   align-items: center;
-  font-size: 4vw;
+  font-size: 9vh;
 }
 a.header-link {
-<<<<<<< HEAD
-  font-size: 3.2vw;
-=======
-    float: right;
-    font-size: 20px;
-    margin-top: 17px;
-    margin-left: 15px;
-    margin-right: 10px;
-    padding: 7px;
-    border: 2px solid black;
-    color: black;
-    text-decoration: none;
+  font-size: 6vh;
 }
-a.header-link:hover {
-    text-decoration: underline;
-}
-#crew-button {
-    background: #80edb9;
-    font-family: IPS-Medium;
-}
-#learn-button {
-    background: #faee38;
-}
-#archive-button {
-    background: #fa385b;
->>>>>>> 7565cda (Use IBM Plex Sans as new font)
-}
-
 
 li.spacer {
   margin-left: 2%;
