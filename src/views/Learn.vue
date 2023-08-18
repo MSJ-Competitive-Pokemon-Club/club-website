d131e75de57d01a2922eba8c2aba7d61b43b27d8
Prabhvir Babra <theprabab@gmail.com>
2023-08-17 20:32:40 -0700

    Create a nice looking sidebar

--- src/views/Learn.vue
+++ src/views/Learn.vue
@@ -16,9 +16,9 @@
         'Items',
         'Secondary Effects',
         'Priority Moves'
-      
-      ],
-        selectedOption: 0
+        ],
+        selectedOption: 0,
+        lessonSidebarClosed: false
       }
       
     },
@@ -34,74 +34,96 @@
 </script>
 
 <template>
-<div id="title">Learn</div>
-<div id="grid-container">
-  <div class="grid-item">
-    <div id="sidebar">
-      <h1>Lessons</h1>
-     <div 
-        v-for="(option, index) in options"
-        :key="index"
-        :class="{ 'option': true, 'selected': selectedOption === index }"
-        @click="() => {
-          selectedOption = index;
-          currentReplayLoc = '/club-website/assets/lesson-replays/'+ option + '.html';
-        }"
-      >
-      {{ option }}
+  <h1 id="title">Learn</h1>
+  <div id="learn-container" :class="{ 'close-sidebar': lessonSidebarClosed }">
+    <div id="sidebar" :class="{ 'hide-sidebar': lessonSidebarClosed }">
+      <h1 id="sidebar-title">📁 Lessons</h1>
+      <div id="lesson-indent">
+        <div
+          v-for="(option, index) in options"
+          :key="index"
+          :class="{ 'option': true, 'selected': selectedOption === index }"
+          @click="() => {
+                  selectedOption = index;
+                  currentReplayLoc = '/club-website/assets/lessons/'+ option + '.html';
+                  currentReplayLocText = '/club-website/assets/text/archive/'+ option + ' Text.html';
+                  }"
+          >
+          {{ option }}
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-<div class="grid-item">
-  <div id="learn-display">
+    <input type="button"
+           :value="(lessonSidebarClosed ? '⇒' : '⇐')"
+           @click="lessonSidebarClosed = !lessonSidebarClosed;"
+           id="sidebar-toggle-button">
+    <div id="replay-display">
       <iframe id="iframe" ref="battle" :src="currentReplayLoc" width="100%" height="600px" frameborder="q0"></iframe>
   </div>
-</div>
-</div>
 </template>
-<style scoped>
-#title{
-  font-size:150px;
-  text-align:center
-}
-#grid-container{
-  display: grid;
-  grid-template-columns: 30% 70%;
-}
-.grid-item {
-  padding: 20px;
-  font-size: 30px;
+<style>
+h1#title{
+  font-size: 4vw;
   text-align: center;
+  padding-bottom: 2%;
 }
-#sidebar {
-
-    width: 250px;
-    height: 100vh;
-    overflow-y: auto;
-    text-align: center;
-}
-
-#learn-display {
-
+div#learn-container {
   display: grid;
-  width: 100%;
-  height: 100vh;
-  text-align: center;
-  overflow-y: auto;
+  grid-template-columns: 18% 2% 80%;
+  width: 99.5%;
+  transition: grid-template-columns 0.4s ease;
 }
-.option {
-    padding: 10px;
-    font-size: 2vw;
-    cursor: pointer;
-    transition: transform 0.2s ease;
+div#learn-container.close-sidebar {
+  grid-template-columns: 0% 2% 98%;
 }
 
-.option.selected {
-    font-size: 2.5vw; /* Enlarge the selected option */
-    color:orangered
+div#sidebar.sidebar {
+  overflow: hidden;
+  transition: width 0.4s ease;
 }
-.option:hover {
-    transform: scale(1.1);
-    color:red
-  }
+div#sidebar.hide-sidebar {
+  overflow: hidden;
+  width: 0%;
+}
+
+h1#sidebar-title {
+  font-size: 3.2vw;
+  text-align: left;
+  padding-left: 2%;
+}
+
+div.option {
+  font-size: 1.5vw;
+}
+
+div#lesson-indent {
+  margin-left: 5%;
+  padding-left: 5%;
+  border-left: 2px solid black;
+}
+
+div.option {
+  transition: transform 0.2s ease;
+}
+div.option:hover {
+  transform: scale(1.1);
+  color: #826bdb;
+}
+div.option.selected {
+  transform: scale(1.1);
+  font-family: IPS-Medium;
+  color: #f23d3d;
+}
+div.option.selected:after {
+  content: " ↤";
+}
+
+input#sidebar-toggle-button {
+  border: 2px solid black;
+  background: #9ec5f6;
+  font-size: 2vw;
+}
+input#sidebar-toggle-button:hover {
+  border: 2px solid grey;
+}
 </style>
